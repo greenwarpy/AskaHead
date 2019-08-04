2a4d619d8a099e93a1b685952993c2e8c4c0ec65
garionrk <garionrk@gmail.com>
2019-08-05 00:46:02 +1000

    Fixing Branch; This branch for experimenting with the set select screen.

--- app/src/main/java/com/gmail/greenwarpy/askahead/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/gmail/greenwarpy/askahead/ui/main/SectionsPagerAdapter.java
@@ -7,6 +7,7 @@ import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 
 import com.gmail.greenwarpy.askahead.R;
 
